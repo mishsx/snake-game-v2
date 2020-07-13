2d0562a26f47bf0168637a2c566e46474dba45d7
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-13 10:57:32 -0400

    Added update, draw and SNAKE SPEED to snake.js and exported these to game.js

--- game.js
+++ game.js
@@ -1,17 +1,26 @@
-import { SNAKE_SPEED } from "./snake.js";
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
 let lastRenderTime = 0;
 // Creating a game loop
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-
-  console.log("Render");
   lastRenderTime = currentTime;
-}
 
+  update();
+  draw();
+}
 window.requestAnimationFrame(main); // To initiate our loop for first time.
 
-function update() {}
+function update() {
+  updateSnake();
+}
 
-function draw() {}
+function draw() {
+  drawSnake();
+}
--- snake.js
+++ snake.js
@@ -1,1 +1,9 @@
 export const SNAKE_SPEED = 1; //Controls the speed of the snake;
+
+export function update() {
+  console.log("update snake");
+}
+
+export function draw() {
+  console.log("draw snake");
+}
