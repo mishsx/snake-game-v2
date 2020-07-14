0aa4c4d02fb37e307f8af5cd7a54a2f49fd14261
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-14 04:04:14 -0400

    Added logic to handle what happens when snake consumes food

--- snake.js
+++ snake.js
@@ -2,6 +2,7 @@ import { getInputDirection } from "./input.js";
 
 export const SNAKE_SPEED = 1; //Controls the speed of the snake;
 const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
 
 export function update() {
   const inputDirection = getInputDirection();
@@ -20,4 +21,18 @@ export function draw(gameBoard) {
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
   });
+}
+
+export function expandSnake(amount) {
+  newSegment += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some((segment) => {
+    return equalPositions(segment, position);
+  });
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
 }
